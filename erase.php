23977a71dfe97062a4fc1fc0a3aaf08c945197af
Nexon Veloza <134989463+Nedavefo@users.noreply.github.com>
2024-04-05 19:35:49 -0400

    Update erase.php

--- erase.php
+++ erase.php
@@ -1,46 +1,37 @@
 <?php
-session_start(); // Iniciar sesión para acceder a las variables de sesión
+session_start(); 
 
-// Verificar si se ha enviado un formulario para eliminar una publicación
 if(isset($_POST["delete_image"]) && isset($_POST["image_id"])) {
-    $image_id = $_POST["image_id"]; // ID de la imagen a eliminar
+    $image_id = $_POST["image_id"]; 
 
-    // Obtener el nombre de usuario de la sesión
-    $logged_in_user_id = $_SESSION['username']; // Suponiendo que has almacenado el nombre de usuario en $_SESSION['username'] después de iniciar sesión
-
-    // Verificar si el usuario actual es el propietario de la imagen
-    $servername = "localhost"; // Cambia esto según tu configuración
-    $username = "root"; // Cambia esto según tu configuración
-    $password = ""; // Cambia esto según tu configuración
+    $logged_in_user_id = $_SESSION['username'];    
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
     $dbname = "assignment2";
 
-    // Crear conexión
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    $message = ""; // Variable para almacenar el mensaje
+    $message = ""; 
 
-    // Verificar conexión
     if ($conn->connect_error) {
         die("Error de conexión: " . $conn->connect_error);
     }
 
-    // Sentencia SQL para obtener la información de la imagen
     $sql_get_image_info = "SELECT filename, uploaded_by FROM tbl_images WHERE id = $image_id";
     $result = $conn->query($sql_get_image_info);
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        $filename = $row["filename"]; // Nombre del archivo de imagen
-        $image_owner_id = $row["uploaded_by"]; // ID del usuario que subió la imagen
+        $filename = $row["filename"]; 
+        $image_owner_id = $row["uploaded_by"]; 
 
-        // Verificar si el usuario actual es el propietario de la imagen
         if ($logged_in_user_id == $image_owner_id) {
-            // Eliminar la publicación de la base de datos
             $sql_delete_image = "DELETE FROM tbl_images WHERE id = $image_id";
             if ($conn->query($sql_delete_image) === TRUE) {
-                // Eliminar el archivo de imagen del servidor
-                $target_dir = "uploads/"; // Directorio donde se guardan las imágenes subidas
-                $target_path = $target_dir . $filename; // Ruta completa del archivo de imagen
-                if (unlink($target_path)) { // Eliminar el archivo de imagen
+                
+                $target_dir = "uploads/"; 
+                $target_path = $target_dir . $filename; 
+                if (unlink($target_path)) { 
                     $message =  "La publicación ha sido eliminada correctamente.";
                 } else {
                     $message =  "Error al eliminar el archivo de imagen del servidor.";
