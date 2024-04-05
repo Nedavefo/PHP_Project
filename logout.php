f55fa2bb01a9912cdf015605feaeaaa6e45ad36a
Nexon Veloza <134989463+Nedavefo@users.noreply.github.com>
2024-04-05 19:38:57 -0400

    logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,10 @@
 <?php
-// Iniciar la sesión
 session_start();
 
-// Destruir todas las variables de sesión
 session_unset();
 
-// Destruir la sesión
 session_destroy();
 
-// Redireccionar al usuario a una página de inicio de sesión u otra página deseada
 header("Location: login.php");
 exit;
 ?>
